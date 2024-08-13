b497c51180eebcce8a7185bf57b95d23eb09e809
karishma-c <karishmadgct@gmail.com>
2024-08-13 16:20:53 +0530

    added type to use es modules

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-const generateRandomNumber = require('./utils');
+const { generateRandomNumber, celciusToFarenheit } = require('./utils');
 
 console.log(`Random Number: ${generateRandomNumber()}`);
+console.log(`Celcius To Farenheit: ${celciusToFarenheit(10)}`)
