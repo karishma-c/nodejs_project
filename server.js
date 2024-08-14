1aa01d3a324c2061c99a445a2e83cfa84ad997ac
karishma-c <karishmadgct@gmail.com>
2024-08-14 19:18:37 +0530

    created a response with status code and error message object

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ import http from 'http';
 const PORT = 8000;
 
 const server = http.createServer((req,res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World!</h1>');
+    // res.setHeader('Content-Type', 'text/html');
+    // res.end('<h1>Hello World!</h1>');
+    
+    res.writeHead(500, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Server Error' }))
 });
 
 server.listen(PORT, () => {
