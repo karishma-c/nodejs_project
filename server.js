e140a933b6181b03ac8952ca0a9312e41a6c1558
karishma-c <karishmadgct@gmail.com>
2024-08-14 19:45:58 +0530

    created env file for environment variables

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 const server = http.createServer((req,res) => {
     // res.setHeader('Content-Type', 'text/html');
     // res.end('<h1>Hello World!</h1>');
     
-    res.writeHead(500, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({ message: 'Server Error' }))
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello World!</h1>');
 });
 
 server.listen(PORT, () => {
